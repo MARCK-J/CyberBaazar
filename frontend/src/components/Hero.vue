045ca967d2d8363a072fa7d178df4acebe178e80
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2023-10-29 19:11:23 -0400

    creacion correcta de login auth0

---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero-container">
+    <div class="text-center hero">
+      <h1 class="mb-4">CyberBaazar</h1>
+      
+      <p>This is the home page of CyberBaazar.</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeroContent"
+};
+</script>
+
+<style scoped>
+.hero-container {
+  background-color: black;
+  color: white;
+  height: 100vh;
+  display: flex;
+  flex-direction: ;
+  justify-content: center;
+  align-items: center;
+}
+
+.hero p {
+  font-size: 15px;
+}
+</style>
