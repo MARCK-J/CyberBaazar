b156e2f18dfb50a50e03138ca54cc496fa77c40a
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2023-11-12 20:12:39 -0400

    creacion sping

---
+++ backend/src/main/java/ucb/edu/bo/SIS213/Cyberbaazar/CyberBaazar/CyberBaazarApplication.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.SIS213.Cyberbaazar.CyberBaazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberBaazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberBaazarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ucb/edu/bo/SIS213/Cyberbaazar/CyberBaazar/CyberBaazarApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.bo.SIS213.Cyberbaazar.CyberBaazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberBaazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
