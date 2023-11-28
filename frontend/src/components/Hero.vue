626070784c21d2392c2d6c4803468fd18ea785e2
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2023-11-28 09:13:41 -0400

    modificacion dependencias

--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -4,13 +4,46 @@
       <h1 class="mb-4">CyberBaazar</h1>
       
       <p>This is the home page of CyberBaazar.</p>
+
+      <pre>{{ responseData }}</pre>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
+  
 export default {
-  name: "HeroContent"
+  data() {
+    return {
+      responseData: null,
+    };
+  },
+  mounted() {
+    this.pollApi();
+  },
+  methods: {
+    async pollApi() {
+      const job_id = 'el_id_de_tu_trabajo'; // Reemplaza esto con el ID real de tu trabajo
+      const url = `https://price-analytics.p.rapidapi.com/poll-job/6564ef348de040452f26e0a9`;
+
+      const options = {
+        method: 'GET',
+        url,
+        headers: {
+          'X-RapidAPI-Key': '7f26c57ed7msh1b75ef0bb587240p1e019djsn66f587f31b02',
+          'X-RapidAPI-Host': 'price-analytics.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await axios.request(options);
+        this.responseData = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
 
